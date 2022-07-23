efb78fd6ca0b91218ecd082041862d0ccae1ba15
Willy Weiskopf <william@weiskopf.me>
2022-07-23 13:49:25 -0400

    Typo missing character.

--- keyboards/splitkb/kyria/keymaps/dartagan-via/keymap.c
+++ keyboards/splitkb/kyria/keymaps/dartagan-via/keymap.c
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_QUOTE,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                 KC_NO ,  C_LGUI,   NAV  , ALT_ENT,  KC_NO,     KC_NO, KC_SPC, SYM , KC_RCTL, KC_NO
+                                 KC_NO , KC_LGUI,   NAV  , ALT_ENT,  KC_NO,     KC_NO, KC_SPC, SYM , KC_RCTL, KC_NO
     ),
 
 
